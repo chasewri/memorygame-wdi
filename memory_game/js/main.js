5f7a6352f521a1bc426dffb2d584dbc28cf11d80
Chase Wright <chasewri@gmail.com>
2020-04-21 10:43:00 -0600

    ugh, js updated some

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,27 @@
 //console.log('Up and Running!');
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-console.log('User flipped a ' + cardsInPlay[0]);
-cardsInPlay.push(cardTwo);
-console.log('User flipped a ' + cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match!!!');
-  } else {
-    alert('Sorry, try again.');
+
+checkForMatch = () => {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert('You found a match!!!');
+    } else {
+      alert('Sorry, try again.');
+    }
   }
 }
+
+flipCard = (cardId) => {
+  // let cardOne = cards[0];
+  // cardsInPlay.push(cardOne);
+  // console.log('User flipped a ' + cardsInPlay[0]);
+  // let cardTwo = cards[2];
+  // cardsInPlay.push(cardTwo);
+  // console.log('User flipped a ' + cardsInPlay[1]);
+  console.log(`User flipped ${cards[cardId]}, ${cardId}`);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+flipCard(2)
+flipCard(3)
