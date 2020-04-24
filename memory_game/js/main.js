8d1ee1a9ac1fc7a043519419bef8373f90d5b805
Chase Wright <chasewri@gmail.com>
2020-04-23 22:52:23 -0600

    cleaned

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,15 +22,21 @@ let cards = [
   }
 ];
 let cardsInPlay = [];
+
+// introduce the score variable
 let score = 0;
+
+// assign score the previous score before refresh if it is available
 let previousScore = localStorage.getItem('score');
 previousScore = JSON.parse(previousScore);
 if (previousScore != null || previousScore != undefined) {
   score = previousScore;
 }
+// Display the game score in the h3 element using the score variable
 function gameScore() {
   document.getElementById('game-score').innerHTML = `You have found ${score} matches!`;
 }
+// hiding and unhiding the refresh button at the end of a game
 function buttonHidden() {
   document.getElementById('play-again').style.display = 'none';
 }
@@ -38,6 +44,9 @@ function buttonVisibility() {
   document.getElementById('play-again').style.display = 'inline';
 }
 
+// checking to see if the cards are a match
+// adding one to the score if it is a match
+// buttonVisibility if it is a match or not
 function checkForMatch() {
   if (cardsInPlay.length === 1)
     alert('Please pick another card!')
@@ -55,7 +64,7 @@ function checkForMatch() {
   }
 }
 
-
+// getting cardId, pushing to cardsInPlay, setting new image attribute
 function flipCard() {
   let cardId = this.getAttribute('data-id');
   console.log(cardId)
@@ -66,7 +75,7 @@ function flipCard() {
 }
 
 
-
+// creating the board, hiding the refresh button
 function createBoard() {
   for (let i = 0; i < cards.length; i++) {
     let cardElement = document.createElement('img');
@@ -78,6 +87,7 @@ function createBoard() {
   buttonHidden();
 }
 
+// send item to localStorage and refresh
 function refreshGame() {
   localStorage.setItem('score', JSON.stringify(score));
   location.reload();
