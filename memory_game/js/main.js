407300c0d2bbdfe8bfb60fb3dbb0a4ca4fd63082
Chase Wright <chasewri@gmail.com>
2020-04-26 22:54:59 -0600

    added cards, added random function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,19 +22,29 @@ let cards = [
   }
 ];
 let cardsInPlay = [];
+// chooseCards creates a random array len 4 for createBoard to pull card objects from
+function chooseCards() {
+  const random = [];
+  for (let i = 0; i < 4; i++) {
+    random.push(Math.floor(Math.random()*4));
+  }
+  // console.log(random);
+  return random;
+}
 
 // introduce the score variable
 let score = 0;
 
-// assign score the previous score before refresh if it is available
+// assign score the previous score before the refresh (if it is available)
 let previousScore = localStorage.getItem('score');
 previousScore = JSON.parse(previousScore);
 if (previousScore != null || previousScore != undefined) {
   score = previousScore;
 }
-// Display the game score in the h3 element using the score variable
+// Display the game score in an h3 element using the score variable
 function gameScore() {
-  document.getElementById('game-score').innerHTML = `You have found ${score} matches!`;
+  document.getElementById('game-score').innerHTML =
+  `You have found ${score} matches!`;
 }
 // hiding and unhiding the refresh button at the end of a game
 function buttonHidden() {
@@ -43,8 +53,7 @@ function buttonHidden() {
 function buttonVisibility() {
   document.getElementById('play-again').style.display = 'inline';
 }
-
-// checking to see if the cards are a match
+// checking to see if the cards are a match by rank
 // adding one to the score if it is a match
 // buttonVisibility if it is a match or not
 function checkForMatch() {
@@ -68,7 +77,8 @@ function checkForMatch() {
 function flipCard() {
   let cardId = this.getAttribute('data-id');
   console.log(cardId)
-  console.log(`User flipped ${cards[cardId].rank}, ${cardId}, ${cards[cardId].suit}, ${cards[cardId].cardImage}`);
+  console.log(`User flipped ${cards[cardId].rank}, ${cardId}, ${cards[cardId].suit},
+    ${cards[cardId].cardImage}`);
   cardsInPlay.push(cards[cardId].rank);
   this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
@@ -77,10 +87,11 @@ function flipCard() {
 
 // creating the board, hiding the refresh button
 function createBoard() {
-  for (let i = 0; i < cards.length; i++) {
+  let random = chooseCards();
+  for (let i = 0; i < random.length; i++) {
     let cardElement = document.createElement('img');
     cardElement.setAttribute('src', 'images/back.png');
-    cardElement.setAttribute('data-id', i);
+    cardElement.setAttribute('data-id', random[i]);
     cardElement.addEventListener('click', flipCard);
     document.getElementById('game-board').appendChild(cardElement);
   }
@@ -92,6 +103,10 @@ function refreshGame() {
   localStorage.setItem('score', JSON.stringify(score));
   location.reload();
 }
-
+//create 16 randomly selected cards from the 4 that are available in the cards array
 createBoard();
+createBoard();
+createBoard();
+createBoard();
+
 gameScore();
